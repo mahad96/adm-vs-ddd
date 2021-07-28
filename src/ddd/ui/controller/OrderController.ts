ebf173612fbf3cb52a0cac91a97ade9328635eb3
mahad96 <mahad.khan@ssense.com>
2021-07-28 16:55:26 -0400

    modify controller

--- src/ddd/ui/controller/OrderController.ts
+++ src/ddd/ui/controller/OrderController.ts
@@ -1,16 +1,18 @@
 import { CreateOrderService } from '../../application/service/CreateOrderService';
 import { UpdateOrderStatusService } from '../../application/service/UpdateOrderStatusService';
+import { AddItemService } from '../../application/service/AddItemService';
 import { createResponse } from './utils/ResponseBuilder';
-
 
 class OrderController {
 
     private readonly createOrderService: CreateOrderService;
     private readonly updateOrderStatusService: UpdateOrderStatusService;
+    private readonly addItemService: AddItemService;
 
-    public constructor(createOrderService: CreateOrderService, updateOrderStatusService: UpdateOrderStatusService) {
+    public constructor(createOrderService: CreateOrderService, updateOrderStatusService: UpdateOrderStatusService, addItemService: AddItemService) {
         this.createOrderService = createOrderService;
-        this.updateOrderStatusService = updateOrderStatusService
+        this.updateOrderStatusService = updateOrderStatusService;
+        this.addItemService = addItemService;
     }
 
     public async createOrder(req: Request): Promise<Response> {
@@ -23,6 +25,12 @@ class OrderController {
         const { newStatus, id } = req.body;
         const updateStatusResult = await this.updateOrderStatusService.updateStatus(newStatus, id);
         return createResponse(updateStatusResult);
+    }
+
+    public async addItem(req: Request): Promise<Response> {
+        const { sku, quantity, price, id } = req.body;
+        const addItemResult = await this.addItemService.addItem(sku, quantity, price, id);
+        return createResponse(addItemResult);
     }
 
 }
